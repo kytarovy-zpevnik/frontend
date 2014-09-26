b7408922be15839def17b8abc26935d8659881f6
Tomáš Markacz <tomas.markacz@gmail.com>
2014-09-26 10:25:01 +0200

    angular does use strings preceded with # in url for routing, instead of browser's push state (which overwrites whole url)

--- app/FrontendModule.dart
+++ app/FrontendModule.dart
@@ -3,6 +3,7 @@ import 'services/SignInService.dart';
 import 'controllers/SignInController.dart';
 import 'controllers/HomepageController.dart';
 import 'router.dart';
+import 'package:angular/routing/module.dart';
 
 class FrontendModule extends Module {
   static const String API_HOST = 'http://private-4bbe-kytarovyzpevnik.apiary-mock.com';
@@ -11,5 +12,8 @@ class FrontendModule extends Module {
     type(SignInService);
     type(SignInController);
     type(HomepageController);
+
+    bind(RouteInitializerFn, toValue: RouterInitializer);
+    bind(NgRoutingUsePushState, toValue: new NgRoutingUsePushState.value(false));
   }
 }
