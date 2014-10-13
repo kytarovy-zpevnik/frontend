1153bc47b5e4b75bbbf85de1aa46667300fb5108
Tomáš Markacz <tomas.markacz@gmail.com>
2014-10-13 15:12:45 +0200

    user registration

---
+++ app/controllers/register_controller.dart
@@ -0,0 +1,31 @@
+part of app;
+
+@Controller(selector: '[register]', publishAs: 'ctrl')
+class RegisterController {
+  final Router _router;
+  final MessageService _messageService;
+  final UserResource _userResource;
+
+  String username;
+  String email;
+  String password;
+
+  RegisterController(this._router, this._messageService, this._userResource);
+
+  void register() {
+    _userResource.create(username, email, password).then((_) {
+      _messageService.addSuccess('Zaregistrován.', 'Registrace nového uživatele byla úspěšná, nyní se můžete přihlásit.');
+      _router.go('homepage', {});
+    }).catchError((ApiError e) {
+      switch (e.error) {
+        case 'DUPLICATE_USERNAME':
+          _messageService.addError('Uživatelské jméno obsazeno.', 'Uživatel se zadaným uživatelským jménem je již registrován.');
+          break;
+
+        case 'DUPLICATE_EMAIL':
+          _messageService.addError('Email zaregistrován.', 'Uživatel se zadaným emailem je již registrován.');
+        break;
+      }
+    });
+  }
+}
---
+++ app/resources/user_resource.dart
@@ -0,0 +1,45 @@
+part of app;
+
+@Injectable()
+class UserResource {
+  final Api _api;
+
+  UserResource(this._api);
+
+  Future<User> create(String username, String email, String password) {
+    return _api.post('users', data: {
+      'username': username,
+      'email': email,
+      'password': password
+    }).then((HttpResponse response) {
+      var role = new Role(response.data['role']['id'], response.data['role']['slug'], response.data['role']['name']);
+      var user = new User(response.data['id'], response.data['username'], response.data['email'], role, response.data['lastLogin']);
+      return new Future.value(user);
+    });
+  }
+
+  Future<List<User>> readAll() {
+    return _api.get('users').then((HttpResponse response) {
+      var users = response.data.map((data) {
+        var role = new Role(data['role']['id'], data['role']['slug'], data['role']['name']);
+        var user = new User(data['id'], data['username'], data['email'], role, data['lastLogin']);
+        return user;
+      });
+
+      return new Future.value(users);
+    });
+  }
+
+  Future update(User user) {
+    return _api.put('users/' + user.id.toString(), data: {
+      'role': {
+        'slug': user.role.slug
+      }
+    }).then((HttpResponse response) {
+      user.role.id   = response.data['role']['id'];
+      user.role.slug = response.data['role']['slug'];
+      user.role.name = response.data['role']['name'];
+      return new Future.value(user);
+    });
+  }
+}
