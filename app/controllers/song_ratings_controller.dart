f907a4fcd8cb2cd4dc85d31f1e5508e41b9a54fb
jirastom <jirasek.tomas08@gmail.com>
2014-11-13 12:22:09 +0100

    User can't rate his song or songbook

--- app/controllers/song_ratings_controller.dart
+++ app/controllers/song_ratings_controller.dart
@@ -5,15 +5,21 @@ class SongRatingsController {
 
   final SongRatingResource _ratingResource;
   final SongsResource _songsResource;
+  SessionService _sessionService;
   RouteProvider _routeProvider;
 
   List ratings = [];
 
+  User user;
   Song song;
 
-  SongRatingsController(this._ratingResource, this._songsResource, this._routeProvider) {
+  SongRatingsController(this._sessionService, this._ratingResource, this._songsResource, this._routeProvider) {
     _songsResource.read(_routeProvider.parameters['id']).then((Song song){
       this.song = song;
+      _sessionService.initialized.then((_) {
+        user = _sessionService.session.user;
+        this.user = new User(user.id, user.username, user.email, user.role, user.lastLogin);
+      });
     });
     _ratingResource.readAllRating(_routeProvider.parameters['id']).then(_processRatings);
   }
--- app/controllers/songbook_ratings_controller.dart
+++ app/controllers/songbook_ratings_controller.dart
@@ -5,15 +5,21 @@ class SongbookRatingsController {
 
   final SongbookRatingResource _ratingResource;
   final SongbooksResource _songbooksResource;
+  SessionService _sessionService;
   RouteProvider _routeProvider;
 
   List ratings = [];
 
+  User user;
   Songbook songbook;
 
-  SongbookRatingsController(this._ratingResource, this._songbooksResource, this._routeProvider) {
+  SongbookRatingsController(this._sessionService, this._ratingResource, this._songbooksResource, this._routeProvider) {
     _songbooksResource.read(_routeProvider.parameters['id']).then((Songbook songbook){
       this.songbook = songbook;
+      _sessionService.initialized.then((_) {
+        user = _sessionService.session.user;
+        this.user = new User(user.id, user.username, user.email, user.role, user.lastLogin);
+      });
     });
     _ratingResource.readAllRating(_routeProvider.parameters['id']).then(_processRatings);
   }
