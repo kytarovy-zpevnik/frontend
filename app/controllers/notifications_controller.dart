f12e1c53477c14bd99141e6fd624532d0fad3419
mantljir <george.mantlik@gmail.com>
2016-01-19 14:57:44 +0100

    Change notifications page
    Use NotificationService, change visual aspect, notification is read only after clicking it or special button

--- app/controllers/notifications_controller.dart
+++ app/controllers/notifications_controller.dart
@@ -2,19 +2,23 @@ part of app;
 
 @Controller(selector: '[notifications]', publishAs: 'ctrl')
 class NotificationsController {
-  final NotificationsResource _notificationsResource;
+  final NotificationService _notificationService;
 
-  List<Notification> notifications = [];
+  //List<Notification> notifications = [];
 
   var formatter = new DateFormat('d. M. yyyy H:m');
 
-  NotificationsController(this._notificationsResource) {
-    querySelector('html').classes.add('wait');
-    _notificationsResource.readAll().then((notifications) {
-      notifications.forEach((notification) {
-        this.notifications.add(notification);
-      });
-      querySelector('html').classes.remove('wait');
-    });
+  NotificationsController(this._notificationService);
+
+  List<Notification> get notifications => _notificationService.notifications;
+
+  void readNotification(Notification notification) {
+    if (!notification.read) {
+      _notificationService.readNotification(notification);
+    }
+  }
+
+  void readAllNotifications() {
+    _notificationService.readAllNotifications();
   }
 }
